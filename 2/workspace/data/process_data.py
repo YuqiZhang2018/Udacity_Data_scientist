c2ab3bd4be56c0674f26117be96ec6edc48d4e89
YuqiZhang2018 <1771061465@qq.com>
2023-12-13 09:52:11 +0800

    modify program 2 .py files

--- 2/workspace/data/process_data.py
+++ 2/workspace/data/process_data.py
@@ -1,190 +1,66 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # ETL Pipeline Preparation
-# Follow the instructions below to help you create your ETL pipeline.
-# ### 1. Import libraries and load datasets.
-# - Import Python libraries
-# - Load `messages.csv` into a dataframe and inspect the first few lines.
-# - Load `categories.csv` into a dataframe and inspect the first few lines.
-
-# In[2]:
-
-
-# import libraries
+import sys
 import pandas as pd
-
 from sqlalchemy import create_engine
 
 
-# In[11]:
-
-
-def load_datasets(dataset_name):
-    dataset = pd.read_csv(dataset_name)
-    print(dataset.head())
-    return dataset
-
-
-# In[12]:
-
-
-# load messages dataset and 
-messages = load_datasets("messages.csv")
-
-
-# In[13]:
-
-
-# load categories dataset
-categories = load_datasets("categories.csv")
-
-
-# ### 2. Merge datasets.
-# - Merge the messages and categories datasets using the common id
-# - Assign this combined dataset to `df`, which will be cleaned in the following steps
-
-# In[20]:
-
-
-def merge_dataset(dataset1,dataset2,connection):
-    df = pd.merge(dataset1, dataset2, on=connection)
-    print(df.head())
-    df.to_csv('output.csv', index=False)
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on='id')
     return df
 
 
-# In[21]:
+def clean_data(df):
+    # Split `categories` into separate category columns
+    categories = df['categories'].str.split(';', expand=True)
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames
+
+    # Convert category values to just numbers 0 or 1
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(int)
+
+    # Replace `categories` column in df with new category columns
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.concat([df, categories], axis=1)
+
+    # Remove duplicates
+    df.drop_duplicates(inplace=True)
+    return df
 
 
-df = merge_dataset(categories,messages,"id")
+def save_data(df, database_filename):
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('DisasterResponse', engine, index=False)
 
 
-# ### 3. Split `categories` into separate category columns.
-# - Split the values in the `categories` column on the `;` character so that each value becomes a separate column. You'll find [this method](https://pandas.pydata.org/pandas-docs/version/0.23/generated/pandas.Series.str.split.html) very helpful! Make sure to set `expand=True`.
-# - Use the first row of categories dataframe to create column names for the categories data.
-# - Rename columns of `categories` with new column names.
+def main():
+    if len(sys.argv) == 4:
 
-# In[22]:
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
 
-def split_columes(dataset,column,split_signal):
-    # create a dataframe of the 36 individual category columns
-    dataset = dataset[column].str.split(split_signal, expand=True)
-    print(dataset.head())
-    
-    # select the first row of the categories dataframe    
-    row = dataset.iloc[0]
+        print('Cleaning data...')
+        df = clean_data(df)
 
-    # use this row to extract a list of new column names for categories.
-    # one way is to apply a lambda function that takes everything 
-    # up to the second to last character of each string with slicing
-    dataset_colnames = row.apply(lambda x: x[:-2])
-    print(dataset_colnames)
-    
-    # rename the columns of `categories`
-    dataset.columns = dataset_colnames
-    dataset.head()
-    return dataset
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+
+        print('Cleaned data saved to database!')
+
+    else:
+        print('Please provide the filepaths of the messages and categories ' \
+              'datasets as the first and second argument respectively, as ' \
+              'well as the filepath of the database to save the cleaned data ' \
+              'to as the third argument. \n\nExample: python process_data.py ' \
+              'disaster_messages.csv disaster_categories.csv ' \
+              'DisasterResponse.db')
 
 
-# In[25]:
-
-
-categories = split_columes(df,"categories",";")
-
-
-# ### 4. Convert category values to just numbers 0 or 1.
-# - Iterate through the category columns in df to keep only the last character of each string (the 1 or 0). For example, `related-0` becomes `0`, `related-1` becomes `1`. Convert the string to a numeric value.
-# - You can perform [normal string actions on Pandas Series](https://pandas.pydata.org/pandas-docs/stable/text.html#indexing-with-str), like indexing, by including `.str` after the Series. You may need to first convert the Series to be of type string, which you can do with `astype(str)`.
-
-# In[27]:
-
-
-def convert_value(data):
-    for column in data:
-        # set each value to be the last character of the string
-        data[column] =  data[column].astype(str).str[-1]
-
-        # convert column from string to numeric
-        data[column] = data[column].astype(int)
-    print(data.head())
-    return data
-
-
-# In[28]:
-
-
-categories = convert_value(categories)
-
-
-# ### 5. Replace `categories` column in `df` with new category columns.
-# - Drop the categories column from the df dataframe since it is no longer needed.
-# - Concatenate df and categories data frames.
-
-# In[29]:
-
-
-def categories_replace(dataset1,column, dataset2):
-    # drop the original categories column from `df`
-    dataset1.drop('categories', axis=1, inplace=True)
-    print(dataset1.head())
-    
-    # concatenate the original dataframe with the new `categories` dataframe
-    dataset1 = pd.concat([dataset1, dataset2], axis=1)
-    dataset1.head()   
-    
-    return dataset1
-
-
-# In[30]:
-
-
-df = categories_replace(df, 'categories',categories)
-
-
-# ### 6. Remove duplicates.
-# - Check how many duplicates are in this dataset.
-# - Drop the duplicates.
-# - Confirm duplicates were removed.
-
-# In[33]:
-
-
-def remove_duplicate(dataset):
-    # check number of duplicates
-    print(dataset.duplicated().sum())
-
-    # drop duplicates
-    dataset.drop_duplicates(inplace=True)
-
-    # check number of duplicates
-    print(dataset.duplicated().sum())
-    
-    return dataset
-
-
-# In[34]:
-
-
-df = remove_duplicate(df)
-
-
-# ### 7. Save the clean dataset into an sqlite database.
-# You can do this with pandas [`to_sql` method](https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.to_sql.html) combined with the SQLAlchemy library. Remember to import SQLAlchemy's `create_engine` in the first cell of this notebook to use it below.
-
-# In[36]:
-
-
-engine = create_engine('sqlite:///InsertDatabaseName.db')
-df.to_sql('InsertTableName1', engine, index=False)
-
-
-# ### 8. Use this notebook to complete `etl_pipeline.py`
-# Use the template file attached in the Resources folder to write a script that runs the steps above to create a database based on new datasets specified by the user. Alternatively, you can complete `etl_pipeline.py` in the classroom on the `Project Workspace IDE` coming later.
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    main()
--- 2/workspace/models/train_classifier.py
+++ 2/workspace/models/train_classifier.py
@@ -1,310 +1,96 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # ML Pipeline Preparation
-# Follow the instructions below to help you create your ML pipeline.
-# ### 1. Import libraries and load data from database.
-# - Import Python libraries
-# - Load dataset from database with [`read_sql_table`](https://pandas.pydata.org/pandas-docs/stable/generated/pandas.read_sql_table.html)
-# - Define feature and target variables X and Y
-
-# In[2]:
-
-
-# import libraries
-from sqlalchemy import create_engine
+import sys
 import pandas as pd
+from sqlalchemy import create_engine
+import re
+import nltk
 
-
-# In[3]:
-
-
-def load_data(database_name, table_name):
-    engine = create_engine(f'sqlite:///{database_name}.db')
-
-    df = pd.read_sql_table(table_name, con=engine)
-   
-    print(df.columns)
-    print(df.head(2))
-
-    X = df['message']
-    Y = df.iloc[:, 4:]
-
-    return X, Y
-
-X, Y = load_data('InsertDatabaseName', 'InsertTableName1')
-
-
-# ### 2. Write a tokenization function to process your text data
-
-# In[6]:
-
-
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+import pickle
+
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('DisasterResponse', engine)
+    X = df['message']
+    Y = df.drop(['id', 'message', 'original', 'genre'], axis=1)
+    category_names = Y.columns
+    return X, Y, category_names
+
 
 def tokenize(text):
-    """
-    Tokenize and clean text by breaking it into words, lemmatizing, 
-    converting to lower case and removing leading/trailing white space.
-
-    Parameters:
-    text (str): Text to be tokenized.
-
-    Returns:
-    list: List of clean, lemmatized tokens.
-    """
-    # Initialize WordNet lemmatizer
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
-    # Tokenize text into words
-    tokens = word_tokenize(text)
-
-    # Initialize an empty list to hold the cleaned tokens
     clean_tokens = []
-
-    # Iterate over each token
-    for token in tokens:
-        # Lemmatize, convert to lower case and remove leading/trailing white space
-        clean_token = lemmatizer.lemmatize(token).lower().strip()
-
-        # Add the clean token to the list
-        clean_tokens.append(clean_token)
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).strip()
+        clean_tokens.append(clean_tok)
 
     return clean_tokens
 
 
-# ### 3. Build a machine learning pipeline
-# This machine pipeline should take in the `message` column as input and output classification results on the other 36 categories in the dataset. You may find the [MultiOutputClassifier](http://scikit-learn.org/stable/modules/generated/sklearn.multioutput.MultiOutputClassifier.html) helpful for predicting multiple target variables.
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
 
-# In[7]:
-
-
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.multioutput import MultiOutputClassifier
-import nltk
-
-# Create a machine learning pipeline
-pipeline = Pipeline([
-    ('vect', CountVectorizer(tokenizer=tokenize)),
-    ('tfidf', TfidfTransformer()),
-    ('clf', MultiOutputClassifier(RandomForestClassifier()))
-])
-
-
-# ### 4. Train pipeline
-# - Split data into train and test sets
-# - Train pipeline
-
-# In[13]:
-
-
-from sklearn.model_selection import train_test_split
-
-def train_classifier(X, Y, pipeline, test_size=0.2, random_state=42):
-    """
-    Train a classifier using a training pipeline.
-
-    Parameters:
-    X: Features dataset.
-    Y: Labels dataset.
-    pipeline: The machine learning pipeline that includes the preprocessing and the classifier.
-    test_size: The proportion of the dataset to include in the test split (default is 0.2).
-    random_state: The seed used by the random number generator (default is 42).
-
-    Returns:
-    The trained pipeline.
-    """
-    # Split data into training and test sets
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=random_state)
-
-    # Train classifier
-    pipeline.fit(X_train, Y_train)
-
-    # Return the trained pipeline and the test set for further evaluation
-    return pipeline, X_train, X_test, Y_train, Y_test
-
-trained_pipeline, X_train, X_test, Y_train, Y_test = train_classifier(X, Y, pipeline)
-
-
-# In[9]:
-
-
-# Split data into training and test sets
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
-
-# Train classifier
-pipeline.fit(X_train, Y_train)
-
-
-# ### 5. Test your model
-# Report the f1 score, precision and recall for each output category of the dataset. You can do this by iterating through the columns and calling sklearn's `classification_report` on each.
-
-# In[11]:
-
-
-import pandas as pd
-from sklearn.metrics import classification_report
-
-def evaluate_model(pipeline, X_test, Y_test):
-    """
-    Predict on test data and print classification report for each label feature.
-
-    Parameters:
-    pipeline: The trained machine learning pipeline.
-    X_test: Test dataset features.
-    Y_test: Test dataset labels.
-
-    Returns:
-    Y_pred_df: DataFrame containing the predictions.
-    """
-    # Predict on test data using the pipeline
-    Y_pred = pipeline.predict(X_test)
-
-    # Get a list of the column names of Y for iteration
-    target_names = Y_test.columns.tolist()
-
-    # Convert Y_test and Y_pred into DataFrames for easier manipulation
-    Y_test_df = pd.DataFrame(Y_test, columns=target_names)
-    Y_pred_df = pd.DataFrame(Y_pred, columns=target_names)
-
-    # Print classification report for each feature
-    for column in target_names:
-        print('------------------------------------------------------\n')
-        print(f'FEATURE: {column}\n')
-        print(classification_report(Y_test_df[column], Y_pred_df[column]))
-
-    return Y_pred_df
-
-Y_pred_df = evaluate_model(trained_pipeline, X_test, Y_test)
-
-
-# ### 6. Improve your model
-# Use grid search to find better parameters. 
-
-# In[15]:
-
-
-from sklearn.model_selection import GridSearchCV
-
-def perform_grid_search(pipeline, X_train, Y_train):
-    """
-    Perform grid search to find the best parameters for the pipeline.
-
-    Parameters:
-    pipeline: The machine learning pipeline on which to perform grid search.
-    X_train: Training dataset features.
-    Y_train: Training dataset labels.
-
-    Returns:
-    cv: The fitted GridSearchCV object.
-    """
-    # Define the parameter grid to search
     parameters = {
-        'clf__estimator__n_estimators': [50, 100],
-        'clf__estimator__min_samples_split': [2, 5],
-        'clf__estimator__min_samples_leaf': [2, 4],
+        'clf__estimator__min_samples_leaf': [2, 4]
     }
 
-    # Create GridSearchCV object with the pipeline, parameter grid, and verbose output
-    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=3)
-
-    # Fit GridSearchCV
-    cv.fit(X_train, Y_train)
-
-    # Return the GridSearchCV object to access the results
+    cv = GridSearchCV(pipeline, param_grid=parameters, n_jobs=-1, verbose=2)
     return cv
 
 
-cv = perform_grid_search(pipeline, X_train, Y_train)
-print(cv.best_params_)
+def evaluate_model(model, X_test, Y_test, category_names):
+    Y_pred = model.predict(X_test)
+    for i in range(len(category_names)):
+        print("Category:", category_names[i], "\n", classification_report(Y_test.iloc[:, i], Y_pred[:, i]))
 
 
-# ### 7. Test your model
-# Show the accuracy, precision, and recall of the tuned model.  
-# 
-# Since this project focuses on code quality, process, and  pipelines, there is no minimum performance metric needed to pass. However, make sure to fine tune your models for accuracy, precision and recall to make your project stand out - especially for your portfolio!
-
-# In[16]:
+def save_model(model, model_filepath):
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
-import pandas as pd
-from sklearn.metrics import classification_report
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
-def predict_and_evaluate(pipeline, X_test, Y_test):
-    """
-    Predict on test data, print classification report for each label, and calculate accuracy.
+        print('Building model...')
+        model = build_model()
 
-    Parameters:
-    pipeline: The trained machine learning pipeline.
-    X_test: Test dataset features.
-    Y_test: Test dataset labels.
+        print('Training model...')
+        model.fit(X_train, Y_train)
 
-    Returns:
-    accuracy: The mean accuracy across all label predictions.
-    """
-    # Predict on test data using the pipeline
-    Y_pred = pipeline.predict(X_test)
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
 
-    # Get a list of the column names of Y for iteration
-    target_names = Y_test.columns.tolist()
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
 
-    # Convert Y_test and Y_pred into DataFrames for easier manipulation
-    Y_test_df = pd.DataFrame(Y_test, columns=target_names)
-    Y_pred_df = pd.DataFrame(Y_pred, columns=target_names)
+        print('Trained model saved!')
 
-    # Print classification report for each feature
-    for column in target_names:
-        print('------------------------------------------------------\n')
-        print('FEATURE: {}\n'.format(column))
-        print(classification_report(Y_test_df[column], Y_pred_df[column]))
-
-    # Calculate and return the mean accuracy across all label predictions
-    accuracy = (Y_pred_df == Y_test_df).mean()
-    return accuracy
+    else:
+        print('Please provide the filepath of the disaster messages database ' \
+              'as the first argument and the filepath of the pickle file to ' \
+              'save the model to as the second argument. \n\nExample: python ' \
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
-accuracy = predict_and_evaluate(pipeline, X_test, Y_test)
-print('Mean Accuracy:', accuracy)
-
-
-# ### 8. Try improving your model further. Here are a few ideas:
-# * try other machine learning algorithms
-# * add other features besides the TF-IDF
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# ### 9. Export your model as a pickle file
-
-# In[28]:
-
-
-import pickle
-
-with open('model.pkl', 'wb') as file:
-    pickle.dump(cv, file)
-    
-
-
-# ### 10. Use this notebook to complete `train.py`
-# Use the template file attached in the Resources folder to write a script that runs the steps above to create a database and export a model based on a new dataset specified by the user.
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    main()
